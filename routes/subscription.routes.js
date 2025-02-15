475408646de7198d34cdfa0b422f3fcfd6c5b430
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-02-15 20:29:45 +0600

    add: added more route for subscription api and access way

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -6,4 +6,32 @@ subscriptionRouter.get('/', (req, res) =>
   res.send({ title: 'GET all subscription' }),
 );
 
+subscriptionRouter.get('/:id', (req, res) =>
+  res.send({ title: 'GET subscription details' }),
+);
+
+subscriptionRouter.post('/', (req, res) =>
+  res.send({ title: 'CREATE subscription' }),
+);
+
+subscriptionRouter.put('/:id', (req, res) =>
+  res.send({ title: 'UPDATE subscription' }),
+);
+
+subscriptionRouter.delete('/:id', (req, res) =>
+  res.send({ title: 'DELETE subscription' }),
+);
+
+subscriptionRouter.get('/user/:id', (req, res) =>
+  res.send({ title: 'GET all user subscriptions' }),
+);
+
+subscriptionRouter.put('/:id/cancel', (req, res) =>
+  res.send({ title: 'GET all subscription' }),
+);
+
+subscriptionRouter.get('/', (req, res) =>
+  res.send({ title: 'GET all subscription' }),
+);
+
 export default subscriptionRouter;
