ce160ba969234fbdd45a57245124dc20d5705a4b
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-02-26 23:00:39 +0600

    fix: fixed a minor issue

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,6 @@
-import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
 import { JWT_EXPIRES_IN, JWT_SECRET } from '../config/env.js';
 import User from '../models/user.model.js';
 
