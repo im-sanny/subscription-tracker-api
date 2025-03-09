ed5f04813a5bd7a79b3b1b9c86a3e9a2b12d6898
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-03-09 22:18:24 +0600

    add: added arcjet middleware functionality

--- middlewares/arcjet.middleware.js
+++ middlewares/arcjet.middleware.js
@@ -1,5 +1,23 @@
-import aj from '../config/arcjet.js'
+import aj from '../config/arcjet.js';
 
-const arcjetMiddleware = (req, res, next) => {
-  
-}
+const arcjetMiddleware = async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req);
+
+    if (decision.inDenied()) {
+      if (decision.reason.isRateLimit())
+        return res.status(429).json({ error: 'Rate limit exceeded' });
+    }
+    if (decision.reason.isBot()) {
+      return res.status(403).json({ error: 'Bot detected' });
+    }
+    return res.status(403).json({ error: 'Access denied' });
+
+    next();
+  } catch (error) {
+    console.log(`Arcjet Middleware Error: ${error}`);
+    next(error);
+  }
+};
+
+export default arcjetMiddleware;
