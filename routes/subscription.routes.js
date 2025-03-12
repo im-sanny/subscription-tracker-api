5e44a5d51219f8abf0695ac513f21b61062ad097
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-03-12 22:26:42 +0600

    create: subscription api created

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { createSubscription } from '../controllers/subscription.controller.js';
+import authorize from '../middlewares/auth.middleware.js';
 
 const subscriptionRouter = Router();
 
@@ -10,9 +12,7 @@ subscriptionRouter.get('/:id', (req, res) =>
   res.send({ title: 'GET subscription details' }),
 );
 
-subscriptionRouter.post('/', (req, res) =>
-  res.send({ title: 'CREATE subscription' }),
-);
+subscriptionRouter.post('/', authorize, createSubscription);
 
 subscriptionRouter.put('/:id', (req, res) =>
   res.send({ title: 'UPDATE subscription' }),
