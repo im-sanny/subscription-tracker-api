2ea68622e36a7ecc757387d13e7b58feb0ac930e
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-03-23 22:03:34 +0600

    add: dayjs and other subcription related func added

--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -1,10 +1,26 @@
+import dayjs from 'dayjs';
 import { createRequire } from 'module';
+import Subscription from '../models/subscription.model.js';
 const require = createRequire(import.meta.url);
 const { serve } = require('@upstash/workflow/express');
 
-export const sendReminders = serve(async () => {
-  const { subscriptionId } = requestPayload;
-  const subscription = await fetchSubscription(subscriptionId);
+export const sendReminders = serve(async (c) => {
+  const { subscriptionId } = c.requestPayload;
+  const subscription = await fetchSubscription(c, subscriptionId);
+
+  if (!subscription || subscription.status !== active) return;
+
+  const renewalDate = dayjs(subscription.renewalDate);
+
+  if (renewalDate.isBefore(dayjs())) {
+    console.log(
+      `Renewal date has passed for subscription ${subscriptionId}. Stopping workflow.`,
+    );
+  }
 });
 
-
+const fetchSubscription = async (c, subscriptionId) => {
+  return await c.run('get subscription', () => {
+    return Subscription.findById(subscriptionId).populate('user', 'name email');
+  });
+};
