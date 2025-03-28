281f793e357d426ebb581dbcccccb74ce4c183b6
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-03-28 21:20:31 +0600

    add: added some func releted to renewal

--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -4,6 +4,8 @@ import Subscription from '../models/subscription.model.js';
 const require = createRequire(import.meta.url);
 const { serve } = require('@upstash/workflow/express');
 
+const REMINDERS = [7, 5, 2, 1];
+
 export const sendReminders = serve(async (c) => {
   const { subscriptionId } = c.requestPayload;
   const subscription = await fetchSubscription(c, subscriptionId);
@@ -16,7 +18,20 @@ export const sendReminders = serve(async (c) => {
     console.log(
       `Renewal date has passed for subscription ${subscriptionId}. Stopping workflow.`,
     );
+    return;
+  }
+
+  for (const daysBefore of REMINDERS) {
+    const reminderDate = renewalDate.subtract(daysBefore, 'day');
+
+    if (reminderDate.isAfter(dayjs())) {
+      
+    }
   }
 });
 
-
+const fetchSubscription = async (c, subscriptionId) => {
+  return await c.run('get subscription', () => {
+    return Subscription.findById(subscriptionId).populate('user', 'name email');
+  });
+};
